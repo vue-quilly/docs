f9d79b29dfb7d0c396c5651a09f1c9e9aec872e3
Oleksandr Shevchuk <34628096+alekswebnet@users.noreply.github.com>
2026-03-19 23:26:52 +0000

    Move documentation

---
+++ .vitepress/cache/deps/vitepress___@vue_devtools-api.js
@@ -0,0 +1,17 @@
+import {
+  addCustomCommand,
+  addCustomTab,
+  onDevToolsClientConnected,
+  onDevToolsConnected,
+  removeCustomCommand,
+  setupDevToolsPlugin
+} from "./chunk-7NEBVMPQ.js";
+export {
+  addCustomCommand,
+  addCustomTab,
+  onDevToolsClientConnected,
+  onDevToolsConnected,
+  removeCustomCommand,
+  setupDevToolsPlugin,
+  setupDevToolsPlugin as setupDevtoolsPlugin
+};
